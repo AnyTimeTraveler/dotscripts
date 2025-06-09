6b212298a12685f8b419ab6470d452c74b0cc7ad
Nora Struck <nora@nora.computer>
2025-06-09 16:24:29 +0200

    WIP: Add template project

---
+++ rust/template/src/main.rs
@@ -0,0 +1,9 @@
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), ErrorMessage> {
+    env_logger::builder()
+        .parse_env(Env::default().filter_or("RUST_LOG", "info"))
+        .target(Target::Stdout)
+        .format_timestamp_secs()
+        .init();
+    Ok(())
+}
