714218bbceeef5f61e0f0a202cdcdf073b539be6
Nora Struck <nora@nora.computer>
2025-06-22 15:54:43 +0200

    Fix nix env

--- shell.nix
+++ shell.nix
@@ -5,14 +5,12 @@ let
   rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
 in
 pkgs.mkShell {
-#  LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-
   nativeBuildInputs = [
-#    pkgs.clang
     pkgs.pkg-config
+
     rustToolchain
   ];
 
-  # run time dependencies
+  # run time dependencies (dynamic linking)
   buildInputs = [ ];
 }
