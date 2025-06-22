969756df69b870f8ad3e8a039aae6ddef8807cc5
Nora Struck <nora@nora.computer>
2025-06-22 15:55:07 +0200

    Add nix scripts for dynamic linking

---
+++ data/shell.nix
@@ -0,0 +1,18 @@
+{
+  pkgs ? import <nixpkgs> { },
+}:
+let
+  rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+in
+pkgs.mkShell {
+  nativeBuildInputs = [
+    pkgs.pkg-config
+
+    rustToolchain
+  ];
+
+  # run time dependencies (dynamic linking)
+  buildInputs = [
+    pkgs.libressl
+  ];
+}
