cb7430356b78e8d8f81ee4e71e92beaaa210671b
Nora Struck <mail@nora.computer>
2026-02-05 00:15:16 +0100

    Implement auto-retry in releaser

--- rust/releaser/src/main.rs
+++ rust/releaser/src/main.rs
@@ -7,6 +7,8 @@ use std::ffi::OsString;
 use std::fs;
 use std::fs::Metadata;
 use std::path::{Path, PathBuf};
+use std::thread::sleep;
+use std::time::Duration;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), ErrorMessage> {
@@ -97,8 +99,14 @@ fn shell(dest_path: &PathBuf) -> Result<(), ErrorMessage> {
     Ok(())
 }
 
-fn copy_files(src_path: &Path, dest_path: &Path, accepted: fn(Metadata, &OsString) -> Result<(), &str>) -> Result<(), ErrorMessage> {
-    for dir in Path::new(&src_path)
+const COPY_RETRY_COUNT: usize = 3;
+
+fn copy_files(
+    src_path: &Path,
+    dest_path: &Path,
+    accepted: fn(Metadata, &OsString) -> Result<(), &str>,
+) -> Result<(), ErrorMessage> {
+    'outer: for dir in Path::new(&src_path)
         .read_dir()
         .with_dyn_err_context(|| format!("Failed to read directory {}", src_path.display()))?
     {
@@ -119,7 +127,19 @@ fn copy_files(src_path: &Path, dest_path: &Path, accepted: fn(Metadata, &OsStrin
         let target_exe_path = dest_path.join(file_name);
 
         info!("Copy {:60} -> {}", path.display(), target_exe_path.display());
-        fs::copy(path, target_exe_path).with_err_context("Error copying file")?;
+        for _ in 0..COPY_RETRY_COUNT {
+            match fs::copy(&path, &target_exe_path).with_err_context("Error copying file") {
+                Ok(_) => continue 'outer,
+                Err(error) => {
+                    eprintln!(
+                        "{}",
+                        ErrorMessage::with_context("Error, retrying in half a second...", error)
+                    );
+                    sleep(Duration::from_millis(500))
+                }
+            }
+        }
+        ErrorMessage::err(format!("Error copying {path:?}, giving up"))?;
     }
     Ok(())
 }
